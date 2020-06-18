301910b52695017bc777c1f331c06ba569adb571
Daniel Richter <daniel.richter@teambank.de>
2020-06-18 22:46:27 +0200

    fix(lib): rename lifecycle steps
    Signed-off-by: Daniel Richter <daniel.richter@teambank.de>

--- core/lib/src/index.test.ts
+++ core/lib/src/index.test.ts
@@ -2,28 +2,28 @@ import {
     emoji,
     parseEmoji,
     release,
-    TypeReader,
-    VersionBumper,
-    VersionDeterminer,
-    VersionReader,
-    VersionRecorder
+    Type,
+    Bump,
+    Determine,
+    Version,
+    Record, Releaser
 } from './index';
 import 'jest-chain';
 
 describe('@bumpup/lib', () => {
     it('release calls each functions excatly once', () => {
-        const getChangeType: TypeReader = jest.fn(lastVersion => 'fix');
-        const getLastVersion: VersionReader = jest.fn(() => '1.0.0');
+        const type: Type = jest.fn(lastVersion => 'fix');
+        const version: Version = jest.fn(() => '1.0.0');
         const getNewVersionFromLastVersion = jest.fn(lastVersion => '1.0.1');
-        const getNewVersion: VersionDeterminer = jest.fn(changeType => getNewVersionFromLastVersion);
-        const bump: VersionBumper = jest.fn(version => ({status: '', message: ''}));
-        const record: VersionRecorder = jest.fn(version => ({status: '', message: ''}));
-        const rel = release(getLastVersion)(getChangeType)(getNewVersion)(bump)(record);
+        const determine: Determine = jest.fn(changeType => getNewVersionFromLastVersion);
+        const bump: Bump = jest.fn(version => ({status: '', message: ''}));
+        const record: Record = jest.fn(version => ({status: '', message: ''}));
+        const rel: Releaser = release(version)(type)(determine)(bump)(record);
         rel();
 
-        expect(getChangeType).toHaveBeenCalledTimes(1);
-        expect(getLastVersion).toHaveBeenCalledTimes(1);
-        expect(getNewVersion).toHaveBeenCalledWith('fix').toHaveBeenCalledTimes(1);
+        expect(type).toHaveBeenCalledTimes(1);
+        expect(version).toHaveBeenCalledTimes(1);
+        expect(determine).toHaveBeenCalledWith('fix').toHaveBeenCalledTimes(1);
         expect(getNewVersionFromLastVersion).toHaveBeenCalledWith('1.0.0').toHaveBeenCalledTimes(1);
         expect(bump).toHaveBeenCalledWith('1.0.1').toHaveBeenCalledTimes(1)
         expect(record).toHaveBeenCalledWith('1.0.1').toHaveBeenCalledTimes(1)
--- core/lib/src/index.ts
+++ core/lib/src/index.ts
@@ -1,32 +1,33 @@
 import {match} from "@bumpup/fp";
 
-export type VersionReader = () => string;
-export type TypeReader = (string) => string;
-export type VersionDeterminer = (string) => (string) => string;
-export type VersionBumper = (string) => { status: string, message: string };
-export type VersionRecorder = (string) => { status: string, message: string }
+export type Version = () => string;
+export type Type = (string) => string;
+export type Determine = (string) => (string) => string;
+export type Bump = (string) => { status: string, message: string };
+export type Record = (string) => { status: string, message: string }
 export type Releaser = () => void;
 
 export const parseEmoji = platform => emoji => platform === "win32" ? '✔' : emoji[0];
 export const emoji = parseEmoji(process.platform);
 
 export const release =
-    (vReader: VersionReader) =>
-        (tReader: TypeReader) =>
-            (determiner: VersionDeterminer) =>
-                (bumper: VersionBumper) => (recorder: VersionRecorder): Releaser => {
-                    return () => {
-                        const lastVersion = vReader();
-                        console.log(`${emoji`📖`} current version is ${lastVersion}`)
-                        const type = tReader(lastVersion);
-                        console.log(`${emoji`🅱`} change type is ${type}`)
-                        const newVersion = determiner(type)(lastVersion);
-                        console.log(`${emoji(`🔎`)} ${newVersion !== null ? `new version is ${newVersion}`: `no new version`}`)
-                        bumper(newVersion)
-                        console.log(`${emoji(`👊`)} bumping version in package.json`)
-                        recorder(newVersion)
-                        console.log(`${emoji(`📌`)} recording version in git`)
-                        console.log(`${emoji(`📦`)} done`)
-                    };
-                }
+    (version: Version) =>
+        (type: Type) =>
+            (determine: Determine) =>
+                (bump: Bump) =>
+                    (record: Record): Releaser => {
+                        return () => {
+                            const lastVersion = version();
+                            console.log(`${emoji`📖`} current version is ${lastVersion}`)
+                            const lastType = type(lastVersion);
+                            console.log(`${emoji`🅱`} change type is ${lastType}`)
+                            const newVersion = determine(lastType)(lastVersion);
+                            console.log(`${emoji(`🔎`)} ${newVersion !== null ? `new version is ${newVersion}` : `no new version`}`)
+                            bump(newVersion)
+                            console.log(`${emoji(`👊`)} bumping version in package.json`)
+                            record(newVersion)
+                            console.log(`${emoji(`📌`)} recording version in git`)
+                            console.log(`${emoji(`📦`)} done`)
+                        };
+                    }
 
