9b90dcfc0492a392cbf937a2eca05242240a11a0
Daniel Richter <daniel.richter@teambank.de>
2020-06-18 21:23:15 +0200

    ci(gh-actions): include build checksum
    Signed-off-by: Daniel Richter <daniel.richter@teambank.de>

--- Gulpfile.mjs
+++ Gulpfile.mjs
@@ -2,7 +2,8 @@ import gulp from 'gulp'
 import * as fs from 'fs';
 import * as path from 'path';
 import * as child_process from 'child_process';
-
+import md5 from 'md5';
+import glob from 'glob';
 
 const builddirs = dir => fs.readdirSync(dir, {withFileTypes: true})
     .filter(direntry => direntry.isDirectory())
@@ -37,6 +38,12 @@ const test = () => {
 const build = () => packagedirs.forEach(doInDir(dir => {
     console.log(child_process.execSync('npm run build').toString());
 }));
+
+const checksum = () => packagedirs.forEach(dir=>{
+    const files = glob.sync(`${dir}/dist/**/*`,{nodir: true});
+    fs.writeFileSync(`${dir}/build.md5sum`, md5(files.map(name=>({name, hash: md5(fs.readFileSync(name))})).sort(file=>file.name).map(file=>file.hash).join(' ')));
+});
+
 const version = () => packagedirs.forEach(doInDir(dir => {
     console.log(child_process.execSync('git log --oneline .').toString());
     console.log(child_process.execSync('npm run version').toString());
@@ -49,5 +56,6 @@ gulp.task('install', task(install));
 gulp.task('ci', task(ci));
 gulp.task('test', task(test));
 gulp.task('build', task(build));
+gulp.task('checksum', task(checksum))
 gulp.task('version', task(version));
 gulp.task('publish', task(publish));
